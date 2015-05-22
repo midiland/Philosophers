e519f46c933c5d04f148bd3bbfa2aec64e830746
Baptiste CRESPIN <bcrespin@e2r6p4.42.fr>
2015-05-22 14:50:26 +0200

    t

---
+++ srcs/main.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apantiez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/05/22 10:50:12 by apantiez          #+#    #+#             */
+/*   Updated: 2015/05/22 14:38:01 by bcrespin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include "libft.h"
+
+
+t_philo			philo[NB_PHILO];
+MUTEX			stick[NB_PHILO];
+
+void		init_philo(t_philo *philo, int place)
+{
+	philo->etats = THINK;
+	philo->life = MAX_LIFE;
+	philo->bagu_r = 0;
+	philo->bagu_l = 1;
+	philo->place = place;
+	philo->thread = NULL;
+}
+
+void		*check_state(void *table)
+{
+	while (((t_table *)table)->philo->life > 0 /*&& ((t_table *)table)->time < TIMEOUT*/)
+	{
+		usleep(400);
+		((t_table *)table)->time += 1;
+		((t_table *)table)->philo->life -= 1;
+		ft_printf("life = %d, time = %d, place = %d\n", ((t_table *)table)->philo->life, ((t_table *)table)->time, ((t_table *)table)->philo->place);
+	}
+	pthread_exit(NULL);
+	return (table);
+}
+
+
+int			main()
+{
+	int		i;
+	t_table table[NB_PHILO];
+
+	i = 0;
+
+	while (i < NB_PHILO)
+	{
+		pthread_mutex_init(&(stick[i]), NULL);
+		init_philo(&philo[i], i);
+		table[i].philo = &philo[i];
+		table[i].time = 0;
+		pthread_create(&(table[i].philo->thread), NULL, check_state, (void*)&(table[i]));
+		sleep(1);
+		i++;
+	}
+	pthread_join(table[0].philo->thread, NULL);
+	return (0);
+}
